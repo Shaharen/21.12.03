7e5f192efb997d4bfc95380d2bc6e99c1996ab40
Choi_J_I <95062692+Shaharen@users.noreply.github.com>
2021-12-04 09:33:57 +0900

    Add files via upload

---
+++ public_protected_default.java
@@ -0,0 +1,13 @@
+
+public class public_protected_default {
+	// * 접근 제한자
+	// public >> protected >> default >> private
+	// public : 어디서나 접근할 수 있음
+	// protected : 같은 패키지 + 상속 관계에서 접근 가능
+	// default : 같은 패키지 내에서만 접근 가능
+	// private : 같은 클래스 내에서만 접근 가능
+	
+	
+	
+	// static : 프로그램 시작시 함께 시작되어라 또는 활성화되어라
+}
